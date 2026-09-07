c2976816bfba13a45318412b11efb00803945a1c
Shark-empire <babor3331@icloud.com>
2026-09-07 09:31:59 +0000

    Add completion script for mitos-box command

---
+++ scripts/completions/mitos-box.fish
@@ -0,0 +1,1 @@
+complete -c mitos-box -f -a "cat ls mkdir rmdir touch cp mv rm ln pwd basename dirname realpath readlink stat echo printf head tail grep sort uniq wc cut tr tee diff ps kill sleep uptime free uname hostname env printenv whoami id groups df du mount umount sync dmesg chmod chown chgrp clear true"
