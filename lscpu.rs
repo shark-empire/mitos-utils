3815f33c1492e13ba63f1f954c84e8bd057c5d01
Shark-empire <babor3331@icloud.com>
2026-09-23 20:11:43 +0000

    Delete lscpu.rs

--- lscpu.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> std::process::ExitCode {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    mitos_utils::common::errors::run(
-        "lscpu",
-        mitos_utils::applets::lscpu::USAGE,
-        args,
-        mitos_utils::applets::lscpu::run,
-    )
-}
