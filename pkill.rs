ae96776e20d0663b475ed4d263cef4137995cc24
Shark-empire <babor3331@icloud.com>
2026-09-23 20:11:00 +0000

    Delete pkill.rs

--- pkill.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> std::process::ExitCode {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    mitos_utils::common::errors::run(
-        "pkill",
-        mitos_utils::applets::pkill::USAGE,
-        args,
-        mitos_utils::applets::pkill::run,
-    )
-}
