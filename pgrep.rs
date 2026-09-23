9e6c9ef90406a86b86ef5159aacdd74d1a343dd9
Shark-empire <babor3331@icloud.com>
2026-09-23 20:11:11 +0000

    Delete pgrep.rs

--- pgrep.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> std::process::ExitCode {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    mitos_utils::common::errors::run(
-        "pgrep",
-        mitos_utils::applets::pgrep::USAGE,
-        args,
-        mitos_utils::applets::pgrep::run,
-    )
-}
